3434c94b0f393007241e88c5cf0d8c57e5257f2f
WeStOn2000 <Tonytany53@gmail.com>
2024-03-23 22:37:11 -0200

    objects

---
+++ js/object.js
@@ -0,0 +1,10 @@
+const person ={
+    name: 'Tim',
+    city: 'Kampala',
+    age: 37,
+    isStudent: true,
+    skills:['javascript','html','css']
+
+};
+const message = `Hi i'm ${person.name}, i live in ${person.city}`;
+console.log(message);
